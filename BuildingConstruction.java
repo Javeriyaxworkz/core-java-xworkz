e7d97b9c1809359cca5a4681a12ed904c55fe1b6
Javeriyaxworkz <Javeriyaxworkz>
2023-07-13 14:14:51 +0530

    Simple methods in Java

---
+++ BuildingConstruction.java
@@ -0,0 +1,30 @@
+class BuildingConstruction{
+
+    static String thingsRequired[] = {"Cement", "Paint", "Mistri", "Wood", "Tails", "Furniture"};	
+	
+    public static void main(String construction[]){
+		getthings();
+	
+	}
+
+
+// using a method
+
+    public static void getthings(){
+		for(String thingRequired : thingsRequired){
+			System.out.println(thingRequired);
+		}
+	}
+
+
+}
+		  // public static void getBuiscuits(){
+			  // System.out.println("Inside get biscuits");
+			  
+			  // for(String biscuit : biscuits) {
+				  // System.out.println(biscuit);
+				  
+				
+			  // }
+			  
+		  // }
