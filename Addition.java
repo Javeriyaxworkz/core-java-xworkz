780c62cc443b502c8dfff3f895353dfa936d4496
Javeriyaxworkz <Javeriyaxworkz>
2023-07-13 14:18:39 +0530

    Simple methods in Java

---
+++ Addition.java
@@ -0,0 +1,24 @@
+class Addition{
+public static void main(String addition[]){
+    
+	add(56, 78);
+	add(36 ,45 , 10);
+	add(10 , 10 , 10);
+	add(96, 99);
+
+
+
+}
+ public static int add(int a, int b){
+		 System.out.println(a + b);
+		 return a + b;
+		 }
+		 
+ public static int add(int a, int b , int c){
+		 System.out.println(a + b + c);
+		 return a + b + c;
+		 }
+
+
+
+}
---
+++ Girias.java
@@ -0,0 +1,23 @@
+class Girias{
+  static String electronicDevices[] = {"refrigerator","Washing Machine","Television","LED","Grinder","AC","IronBox","Microwave"}; 
+   public static void main(String arg[]){
+     System.out.println("main started");
+	
+	 //for
+	 for(int devicesIndex = 0; devicesIndex < electronicDevices.length; devicesIndex++  ){
+	 System.out.println(electronicDevices[devicesIndex]);
+	 
+	 }
+	 System.out.println("main ended");
+   
+   
+   
+   }
+
+
+
+
+
+
+
+}
